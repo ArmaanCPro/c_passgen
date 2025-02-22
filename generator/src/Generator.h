9fe7f2ad1f817669d0c83c5f990d70b970d3c457
armaanc <armaan.c2009@gmail.com>
2025-02-22 11:11:55 -0600

    Removed password_gen struct because it was unnecessary.

--- generator/src/Generator.h
+++ generator/src/Generator.h
@@ -27,34 +27,29 @@ void pp_init(password_policy* policy)
     policy->excluded_chars = "";
 }
 
-typedef struct
-{
-    password_policy* pc;
-} password_gen;
-
-void pg_init(password_gen* pg, password_policy* pc_in)
-{
-    pg->pc = pc_in;
-}
-
 
 // returns a new array that does not contain the chars_to_remove
-char* c_remove_chars(const char* str, const char* chars_to_remove) {
+char* c_remove_chars(const char* str, const char* chars_to_remove) 
+{
     int i, j;
     int str_len = strlen(str);
     int chars_len = strlen(chars_to_remove);
     char* new_str = (char*)malloc(str_len + 1);
     int new_str_index = 0;
 
-    for (i = 0; i < str_len; i++) {
+    for (i = 0; i < str_len; i++) 
+    {
         int should_remove = 0;
-        for (j = 0; j < chars_len; j++) {
-            if (str[i] == chars_to_remove[j]) {
+        for (j = 0; j < chars_len; j++)
+        {
+            if (str[i] == chars_to_remove[j]) 
+            {
                 should_remove = 1;
                 break;
             }
         }
-        if (!should_remove) {
+        if (!should_remove)
+        {
             new_str[new_str_index++] = str[i];
         }
     }
@@ -63,26 +58,26 @@ char* c_remove_chars(const char* str, const char* chars_to_remove) {
     return new_str;
 }
 
-char* pg_generate_advanced(password_gen* pg)
+char* pg_generate_advanced(password_policy* pp)
 {
     char* available_chars = (char*)malloc(sizeof(s_LowerCaseChars) + sizeof(s_UpperCaseChars) + sizeof(s_NumbersChars) + sizeof(s_SymbolsChars) + 1);
-    char* password = (char*)malloc(pg->pc->password_length + 1);
+    char* password = (char*)malloc(pp->password_length + 1);
 
     available_chars[0] = '\0';
 
-    if (pg->pc->require_lc)
+    if (pp->require_lc)
         strcat(available_chars, s_LowerCaseChars);
-    if (pg->pc->require_uc)
+    if (pp->require_uc)
         strcat(available_chars, s_UpperCaseChars);
-    if (pg->pc->require_num)
+    if (pp->require_num)
         strcat(available_chars, s_NumbersChars);
-    if (pg->pc->require_sym)
+    if (pp->require_sym)
         strcat(available_chars, s_SymbolsChars);
 
-    char* filtered_chars = c_remove_chars(available_chars, pg->pc->excluded_chars);
+    char* filtered_chars = c_remove_chars(available_chars, pp->excluded_chars);
     free(available_chars);
 
-    for (size_t i = 0; i < pg->pc->password_length; i++)
+    for (size_t i = 0; i < pp->password_length; i++)
     {
         unsigned char randomIndex;
         randombytes_buf(&randomIndex, sizeof(randomIndex));
@@ -91,7 +86,7 @@ char* pg_generate_advanced(password_gen* pg)
         password[i] = filtered_chars[randomIndex];
     }
 
-    password[pg->pc->password_length] = '\0';
+    password[pp->password_length] = '\0';
 
     free(filtered_chars);
     return password;
--- generator/src/main.c
+++ generator/src/main.c
@@ -6,15 +6,13 @@ int main(int, char*[])
 {
     password_policy pp;
     pp_init(&pp);
-    password_gen pg;
-    pg_init(&pg, &pp);
 
-    char* pwd = pg_generate_advanced(&pg);
+    char* pwd = pg_generate_advanced(&pp);
     printf("password 1: %s\n", pwd);
     free(pwd);
 
     pp.password_length = 100;
-    pwd = pg_generate_advanced(&pg);
+    pwd = pg_generate_advanced(&pp);
     printf("password 2: %s\n", pwd);
     free(pwd);
     pwd = NULL;
